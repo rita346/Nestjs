2c09ff4fa65999d9015f941df835663fab6aec6b
Rita Alam Rita A <rita.alam39@gmail.com>
2022-11-30 12:53:02 +0200

    second commit

--- src/complaints/complaint.service.ts
+++ src/complaints/complaint.service.ts
@@ -73,42 +73,58 @@ export class ComplaintService {
   //get all complaints with descending sort(by createdDate) for admin without filtering status
   async getAllC2() {
     return await this.complaintModel
-      .aggregate()
-      .lookup({
-        from: 'users',
-        localField: 'user',
-        foreignField: '_id',
-        as: 'user',
-      })
-      .project({ __v: 0 })
-      .sort({ createdDate: -1 })
-      .facet({
-        vip: [
-          { $match: { 'user.isVip': true } },
-          { $group: { _id: null, vip: { $push: '$$ROOT' } } },
-        ],
-        nonvip: [
-          { $match: { 'user.isVip': false } },
-          { $group: { _id: null, nonvip: { $push: '$$ROOT' } } },
-        ],
-      })
+      .aggregate([
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'user',
+            foreignField: '_id',
+            as: 'user',
+          },
+        },
+
+        {
+          $sort: {
+            createdDate: -1,
+          },
+        },
+        {
+          $facet: {
+            vip: [
+              {
+                $match: {
+                  'user.isVip': true,
+                },
+              },
+            ],
+            nonVip: [
+              {
+                $match: {
+                  'user.isVip': false,
+                },
+              },
+            ],
+          },
+        },
+      ])
       .project({
-        'vip.vip._id': 0,
         'vip._id': 0,
-        'nonvip._id': 0,
-        'nonvip.nonvip._id': 0,
-        'vip.vip.user.password': 0,
-        'vip.vip.user._id': 0,
-        'vip.vip.user.isVip': 0,
-        'vip.vip.user.isAdmin': 0,
-        'vip.vip.user.createdDate': 0,
-        'vip.vip.user.__v': 0,
-        'nonvip.nonvip.user.password': 0,
-        'nonvip.nonvip.user._id': 0,
-        'nonvip.nonvip.user.isVip': 0,
-        'nonvip.nonvip.user.isAdmin': 0,
-        'nonvip.nonvip.user.createdDate': 0,
-        'nonvip.nonvip.user.__v': 0,
+        'vip.__v': 0,
+        'nonVip._id': 0,
+        'nonVip.nonvip._id': 0,
+        'vip.user.password': 0,
+        'vip.user._id': 0,
+        'vip.user.isVip': 0,
+        'vip.user.isAdmin': 0,
+        'vip.user.createdDate': 0,
+        'vip.user.__v': 0,
+        'nonVip.user.password': 0,
+        'nonVip.user._id': 0,
+        'nonVip.user.isVip': 0,
+        'nonVip.user.isAdmin': 0,
+        'nonVip.user.createdDate': 0,
+        'nonVip.user.__v': 0,
+        'nonVip.__v': 0,
       });
   }
 
@@ -116,104 +132,66 @@ export class ComplaintService {
   async getAllCF2(filterS: GetComStatusDto) {
     const { status } = filterS;
 
-    const groupComp1 = await this.complaintModel
-      .aggregate()
-      .lookup({
-        from: 'users',
-        localField: 'user',
-        foreignField: '_id',
-        as: 'user',
-      })
-      .project({ __v: 0 })
-      .match({ status: status })
-      .sort({ createdDate: -1 })
-      .facet({
-        vip: [
-          { $match: { 'user.isVip': true } },
-          { $group: { _id: null, vip: { $push: '$$ROOT' } } },
-        ],
-        nonvip: [
-          { $match: { 'user.isVip': false } },
-          { $group: { _id: null, nonvip: { $push: '$$ROOT' } } },
-        ],
-      })
+    const grps = await this.complaintModel
+      .aggregate([
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'user',
+            foreignField: '_id',
+            as: 'user',
+          },
+        },
+
+        {
+          $sort: {
+            createdDate: -1,
+          },
+        },
+        {
+          $match: {
+            status: status,
+          },
+        },
+        {
+          $facet: {
+            vip: [
+              {
+                $match: {
+                  'user.isVip': true,
+                },
+              },
+            ],
+            nonVip: [
+              {
+                $match: {
+                  'user.isVip': false,
+                },
+              },
+            ],
+          },
+        },
+      ])
       .project({
-        'vip.vip._id': 0,
         'vip._id': 0,
-        'nonvip._id': 0,
-        'nonvip.nonvip._id': 0,
-        'vip.vip.user.password': 0,
-        'vip.vip.user._id': 0,
-        'vip.vip.user.isVip': 0,
-        'vip.vip.user.isAdmin': 0,
-        'vip.vip.user.createdDate': 0,
-        'vip.vip.user.__v': 0,
-        'nonvip.nonvip.user.password': 0,
-        'nonvip.nonvip.user._id': 0,
-        'nonvip.nonvip.user.isVip': 0,
-        'nonvip.nonvip.user.isAdmin': 0,
-        'nonvip.nonvip.user.createdDate': 0,
-        'nonvip.nonvip.user.__v': 0,
+        'vip.__v': 0,
+        'nonVip._id': 0,
+        'nonVip.nonvip._id': 0,
+        'vip.user.password': 0,
+        'vip.user._id': 0,
+        'vip.user.isVip': 0,
+        'vip.user.isAdmin': 0,
+        'vip.user.createdDate': 0,
+        'vip.user.__v': 0,
+        'nonVip.user.password': 0,
+        'nonVip.user._id': 0,
+        'nonVip.user.isVip': 0,
+        'nonVip.user.isAdmin': 0,
+        'nonVip.user.createdDate': 0,
+        'nonVip.user.__v': 0,
+        'nonVip.__v': 0,
       });
 
-    return groupComp1;
+    return grps;
   }
 }
-
-/*
- //get by filter status for admin in js
- async getAllCF(filterS: GetComStatusDto) {
-  const { status } = filterS;
-  let comp = await this.getAllC();
-  if (status) {
-    comp = comp.filter((comps) => comps.status === status);
-    //if there is a filter using so filter the complaints depends on status passing by query if no return all complaints
-  }
-  return comp;
-}*/
-
-//get all complaints with descending sort(by createdDate) for admin without grouping
-/* async getAllC() {
-  return await this.complaintModel
-    .find({}, { _id: 0, __v: 0 })
-    .sort({ createdDate: 'desc' });
-}
-*/
-
-// vip query alone
-/* const vipgroup = await this.complaintModel
-      .aggregate()
-      .lookup({
-        from: 'users',
-        localField: 'user',
-        foreignField: '_id',
-        as: 'user',
-      })
-      .project({ __v: 0 })
-      .match({ 'user.isVip': true })
-      .group({
-        _id: '$user.isVip',
-        vip: { $push: '$$ROOT' },
-      })
-      .project({ _id: 0 })
-      .sort({ createdDate: 'desc' });
-
-      //noVip query alone
-    const nonvipgroup = await this.complaintModel
-      .aggregate()
-      .lookup({
-        from: 'users',
-        localField: 'user',
-        foreignField: '_id',
-        as: 'user',
-      })
-      .project({ __v: 0 })
-      .match({ 'user.isVip': false })
-      .group({
-        _id: '$user.isVip',
-        nonvip: { $push: '$$ROOT' },
-      })
-      .project({ _id: 0 })
-      .sort({ createdDate: 'desc' });
-
-*/
